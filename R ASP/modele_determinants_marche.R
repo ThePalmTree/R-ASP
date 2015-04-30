d9f4d67453c140bbb32a0b920795a1cece45e6ae
mmartal <matthieu.martal@gmail.com>
2015-04-30 19:53:42 +0200

    Paris winsten

--- R ASP/modele_determinants_marche.R
+++ R ASP/modele_determinants_marche.R
@@ -8,6 +8,7 @@ library(tseries)
 library(MSBVAR)
 library(car)
 library(lmtest)
+library(prais)
 
 #Importation des fichiers R nécéssaires
 source("desaisonnalisation.r")
@@ -122,13 +123,24 @@ dwtest(log(IO1reg_cjo_2)~ log(1+croissance_2)+log(INreg_moy_2))
 
 
 
+# Test prais winsten
+prais.winsten(log(IO1reg_cjo_2)~ log(1+croissance_2)+log(INreg_moy_2), data=mydata)
 
+#Residuals:
+#  Min        1Q    Median        3Q       Max 
+#-0.166216 -0.044708  0.002868  0.038266  0.166035 
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)  
+#Intercept               0.4591     2.7730   0.166   0.8687  
+#log(1 + croissance_2)   2.5416     1.4100   1.803   0.0732 .
+#log(INreg_moy_2)        0.4318     0.3650   1.183   0.2384  
+#---
+#  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+#Residual standard error: 0.06309 on 171 degrees of freedom
+#Multiple R-squared:  0.9748,  Adjusted R-squared:  0.9743 
+#F-statistic:  2201 on 3 and 171 DF,  p-value: < 2.2e-16
 
-
-
-
-
-
+# les pvalues ne sont pas significatives, ce qui montre la limite de l'interprétation précédente
 
 
 
@@ -159,6 +171,11 @@ kpss.test(cbind(IO1reg_cjo[7:210]-beta*INreg_cjo[7:210]))
 # LAG : 3 ; pvalue = 0.01
 # ADF ne permet pas d'exclure la stationnarité de la série IO1-Beta*IN à 10% et donc d'exclure la cointégration
 # KPSS confirme la non stationnarité avec une pvalue très petite (permet de rejeter H0 : la non cointégration)
+# On en déduit qu'il n'y a pas de cointégration entre IO1 et IN, le R2 mesuré est donc significatif
+
+# Remarque 2 : le fait que le R2 soit si bon constitue déjà une bonne preuve en soi de la cointégration
+# Remarque 3 : IN et IO1 devraient logiquement être fortement cointégrés
+
 
 regression = lm(IO1reg_cjo_2 ~ INreg_moy_2 -1)
 summary(regression)
@@ -197,21 +214,23 @@ dwtest(IO1reg_cjo_2 ~ INreg_moy_2 -1)
 
 # H0 est rejeté, on estime les coefficients avec la méthode de Prais-Winsten
 
+prais.winsten(IO1reg_cjo_2 ~ INreg_moy_2 -1, data=mydata)
 
+#Residuals:
+#  Min      1Q  Median      3Q     Max 
+#-9.0490 -1.8767  0.0177  1.7012  8.6963 
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)
+#Intercept   20.792039  15.955644   1.303    0.194
+#INreg_moy_2  0.011347   0.007852   1.445    0.150
+#Residual standard error: 2.9 on 172 degrees of freedom
+#Multiple R-squared:  0.7241,  Adjusted R-squared:  0.7209 
+#F-statistic: 225.7 on 2 and 172 DF,  p-value: < 2.2e-16
+#[[2]]
+#Rho Rho.t.statistic Iterations
+#0.8982654        26.56041          8
 
-
-
-
-
-
-
-
-
-
-# On en déduit qu'il n'y a pas de cointégration entre IO1 et IN, le R2 mesuré est donc significatif
-
-# Remarque 2 : le fait que le R2 soit si bon constitue déjà une bonne preuve en soi de la cointégration
-# Remarque 3 : IN et IO1 devraient logiquement être fortement cointégrés
+# coeffs non significatifs
 
 
 
@@ -277,19 +296,26 @@ dwtest(IO1reg_cjo_2 ~ ratio_conso_INreg_moy_2 + Ratio_IPC_INreg_moy_2 + carbu_IN
 
 # H0 est rejeté, on estime les coefficients avec la méthode de Prais-Winsten
 
+prais.winsten(IO1reg_cjo_2 ~ ratio_conso_INreg_moy_2 + Ratio_IPC_INreg_moy_2 + carbu_INreg_moy_2, data=mydata)
 
 
-
-
-
-
-
-
-
-
-
-
-
+#Residuals:
+#  Min      1Q  Median      3Q     Max 
+#-8.4342 -1.5354 -0.1461  1.3201  9.3650 
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)    
+#Intercept                1.245e+01  7.751e+00   1.606   0.1101    
+#ratio_conso_INreg_moy_2  1.042e-02  1.031e-02   1.010   0.3138    
+#Ratio_IPC_INreg_moy_2    1.347e-02  6.962e-03   1.934   0.0547 .  
+#carbu_INreg_moy_2       -6.242e-05  1.179e-05  -5.294 3.66e-07 ***
+#  ---
+#  Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+#Residual standard error: 2.839 on 170 degrees of freedom
+#Multiple R-squared:  0.9759,  Adjusted R-squared:  0.9754 
+#F-statistic:  1723 on 4 and 170 DF,  p-value: < 2.2e-16
+#[[2]]
+#Rho Rho.t.statistic Iterations
+#0.6014297        9.858966         11
 
 
 
